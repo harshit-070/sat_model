fc436d1702f4b1e1a4659c803cf4abb9506a92ae
Harshit <harshitmitta070@gmail.com>
2023-02-27 08:02:11 +0530

    Language Translation

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
+from typing import List
+from pydantic import BaseModel
+from deep_translator import GoogleTranslator
 
 app = FastAPI()
 
-@app.get('/')
+
+@app.get("/")
 def index():
-    return {"message" : "ok"}
+    return {"message": "ok"}
+
+
+class TranslationRequest(BaseModel):
+    text: str
+    target_langs: str
+
+
+@app.post("/translate")
+async def translate_text(request: TranslationRequest = Body(...)):
+    text = request.text
+    target_langs = request.target_langs
+    tt = GoogleTranslator(source="auto", target=target_langs).translate(text)
+    return {"translate_text": tt}
