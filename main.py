5c29962f41f3836f0c735421ab7e5bdcc7186bf5
Harshit <harshitmitta070@gmail.com>
2023-02-27 16:54:28 +0530

    .

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ import os
 import io
 from dotenv import load_dotenv
 from gtts import gTTS
+import soundfile
+import speech_recognition as sr
+
 
 load_dotenv()
 session_token = os.getenv("CHAT_GPT_SESSION_TOKEN")
@@ -63,3 +66,54 @@ async def translateAudio(request: AudioTranslateRequest = Body(...)):
     audio_file.seek(0)
     headers = {"Content-Disposition": "attachment; filename=speech.mp3"}
     return StreamingResponse(audio_file, headers=headers, media_type="audio/mpeg")
+
+
+class AudioConversionRequest(BaseModel):
+    url: str
+    target_lang: str
+    chatId: str
+
+
+@app.post("/audio_conversion")
+async def audioConversion(request: AudioConversionRequest = Body(...)):
+    # Read the audio file using soundfile
+    data, samplerate = soundfile.read(request.url)
+
+    # Write the audio data to a new file
+    sound_file = f"{request.chatId}.wav"
+    soundfile.write(sound_file, data, samplerate, subtype="PCM_16")
+
+    # Transcribe the audio using speech recognition
+    r = sr.Recognizer()
+    with sr.AudioFile(sound_file) as source:
+        audio_data = r.record(source)
+        text = r.recognize_google(audio_data)
+
+    # Return the resulting transcription
+    # return {"transcription": tt}
+
+
+class AudioTextConversionRequest(BaseModel):
+    url: str
+    target_lang: str
+    chatId: str
+
+
+@app.post("/audio_text")
+async def audioConversion(request: AudioConversionRequest = Body(...)):
+    # Read the audio file using soundfile
+    data, samplerate = soundfile.read(request.url)
+
+    # Write the audio data to a new file
+    sound_file = f"{request.chatId}.wav"
+    soundfile.write(sound_file, data, samplerate, subtype="PCM_16")
+
+    # Transcribe the audio using speech recognition
+    r = sr.Recognizer()
+    with sr.AudioFile(sound_file) as source:
+        audio_data = r.record(source)
+        text = r.recognize_google(audio_data)
+    tt = GoogleTranslator(source="auto", target=request.target_lang).translate(text)
+
+    # Return the resulting transcription
+    return {"transcription": tt}
